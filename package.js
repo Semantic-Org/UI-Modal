ee758f30f36ba6555dce840f41f1b9d7b70d20db
Pusher Robot <jack@semantic-ui.com>
2015-02-18 16:41:07 -0500

    Updated component to version 1.9.0

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ var
 Package.describe({
   name    : 'semantic:ui-modal',
   summary : 'Semantic UI - Modal (official): Single component release of modal',
-  version : '1.9.9',
+  version : '1.9.0',
   git     : 'git://github.com/Semantic-Org/UI-Modal.git',
 });
 
