2f9f741e7e8d048cd3fc37c10729551299db2365
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:16:01 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,15 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-modal',
-  summary : 'Semantic UI - Modal (official): Single component release of modal',
-  version : '1.9.2',
+  summary : 'Semantic UI - Modal: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Modal.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'modal.css',
+    'modal.js'
+  ], 'client');
 });
