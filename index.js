e811b4920f383208ab76c3f10d7b1f0094f0c4d8
Pusher Robot <jack@semantic-ui.com>
2015-08-10 14:44:38 -0400

    Updated component to version 2.0.8

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * # Semantic UI 2.0.7 - Modal
+ * # Semantic UI 2.0.8 - Modal
  * http://github.com/semantic-org/semantic-ui/
  *
  *
@@ -813,7 +813,7 @@ module.exports = function(parameters) {
   ;
 };
 
-module.exports.settings = {
+_module.exports.settings = {
 
   name           : 'Modal',
   namespace      : 'modal',
--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-modal',
   summary : 'Semantic UI - Modal: Single component release',
-  version : '2.0.7',
+  version : '2.0.8',
   git     : 'git://github.com/Semantic-Org/UI-Modal.git',
 });
 
