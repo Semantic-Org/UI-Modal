0286fd2245d96c1932c1e734f00c5d9547ef50ae
Jack Lukic <jack@semantic-ui.com>
2015-12-21 19:32:47 -0500

    Updated component to version 2.1.7

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-modal',
   summary : 'Semantic UI - Modal: Single component release',
-  version : '2.1.6',
+  version : '2.1.7',
   git     : 'git://github.com/Semantic-Org/UI-Modal.git',
 });
 
